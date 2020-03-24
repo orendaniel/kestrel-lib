b736ce3e18dba3747962dce7167f22ae284cfbcc
Oren Daniel <oren_daniel@protonmail.com>
2020-03-24 18:31:45 +0200

    added examples

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
-#Kestrel vision library
+##Kestrel vision library
+
 
 Kestrel is a simple and lightweight computer vision library for the Lua language written in C.
 
-#Why should I not choose Kestrel over X
+##Why should I not choose Kestrel over X
+
 
 Kestrel is designed to be simple and lightweight, as such many features
 that are included in other computer vision libraries are ignored.
 
+
 Furthermore Kestrel is still under development and is currently unstable.
  
 
-#Dependencies
+##Dependencies
 
 libv4l
 
 lua 5.3
 
 
-#TODO
+
+##TODO
+
 
 -document the source code / check memory leaks / debugging
 
---
+++ example/example.lua
@@ -0,0 +1,41 @@
+--[[ 
+A basic example
+]]--
+
+-- import kestrel with
+kestrel = require "kestrel"
+
+-- open camera set resolution to 160x120
+-- you can set other properties such as FPS or brightness using v4l2-ctl
+cam = kestrel.opendevice("/dev/video0", 160, 120)
+
+for n=1,100 do
+	local st = os.clock()
+	
+	-- read frame from camera
+	local img = cam:readframe()
+
+	-- convert frame to hsv
+	local hsv = kestrel.rgb_to_hsv(img)
+
+	-- make a binary thershold image
+	local bin = hsv:inrange({0, 0, 40}, {255, 255, 255})
+
+	-- get image contours
+	local contours = kestrel.findcontours(bin)
+
+	print("found " .. tostring(#contours) .. " contours")
+
+	for i, cnt in pairs(contours) do
+		-- calculate the center and area of each contour
+		center = cnt:center()
+		area = cnt:area()
+		print("#" .. tostring(i), cnt:area(), center[1], center[2])
+	end
+
+	print("process time: ", os.clock() - st)
+	print("---------------")
+end
+
+-- close the camera
+cam:close()
